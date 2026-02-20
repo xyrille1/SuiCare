14bee6d862c91a1da9d7d604ac6ff72fe66fb025
Xyrille Navora <xyrillenavora5@gmail.com>
2026-02-20 12:51:35 +0800

    fully working

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: process.cwd(),
   transpilePackages: ['@mysten/dapp-kit', '@mysten/sui.js'],
   typescript: {
     ignoreBuildErrors: true,
